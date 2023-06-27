955b993364fee2deca4fee369f23ba78b345e287
Stanly Shianovskiy <st-small@rambler.ru>
2023-06-27 19:29:53 +0300

    Initial Commit

---
+++ SwiftUITableViewExample/SwiftUITableViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableViewExampleApp.swift
+//  SwiftUITableViewExample
+//
+//  Created by Stanly Shiyanovskiy on 27.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
